40cfdee2c20b652b67fccad209122c44304313b9
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2022-11-18 21:53:43 +0000

    poprawka

--- src/HalloJava.java
+++ src/HalloJava.java
@@ -1,5 +1,8 @@
 public class HalloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java");
-    }
+        System.out.println("Hello Java X2");
+        }
 }
+
+
